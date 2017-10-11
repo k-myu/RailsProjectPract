f33195087115bff1fd0dc8714b5ad63c39af1ea0
MarinaMelnikova <marmel4578@gmail.com>
2017-10-11 23:16:30 +0300

    ll

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'main/index'
+
+  get 'main/help'
+
+  get 'main/contacts'
+
+  get 'main/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
